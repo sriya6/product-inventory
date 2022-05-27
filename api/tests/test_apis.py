2ce875d93577e5a0f985c58c1f137cff45e9e5f3
sriya1996 <agarwalsriya67@gmail.com>
2022-05-27 12:43:40 +0530

    Updated the pending tests and updated views

--- api/tests/test_apis.py
+++ api/tests/test_apis.py
@@ -1,6 +1,7 @@
 import json
+import uuid
 from rest_framework import status
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from products.models import Product
 from ..serializers import ProductSerializer
@@ -29,17 +30,107 @@ class getAllProductsTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 class getSingleProductTest(TestCase):
-    # TODO
-    pass
+    def setUp(self):
+        self.item1 = Product.objects.create(
+            name="Test Item1", price=250, quantity=4, category="Stationary")
+        self.item2 = Product.objects.create(
+            name="Test Item2", price=200, quantity=4, category="Toys")
+
+    def test_get_single_product(self):
+        response = client.get(
+            reverse('single-product', kwargs={'pk': self.item2.pk}))
+        product = Product.objects.get(pk=self.item2.pk)
+        serializer = ProductSerializer(product)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_product(self):
+        random_uuid = str(uuid.uuid4())
+        response = client.get(
+            reverse('single-product', kwargs={'pk': random_uuid}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 class createNewProductTest(TestCase):
-    # TODO
-    pass
 
-class updateSingleProduct(TestCase):
-    # TODO
-    pass
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Test Item',
+            'description': 'create new product to test',
+            'quantity': 12,
+            'price': 100,
+            'category': 'Grocery'
+        }
+        self.invalid_payload = {
+            'name': '',
+            'quantity': 10,
+            'category': 'Electronics',
+            'price': 500
+        }
+
+    def test_create_valid_product(self):
+        response = client.post(
+            reverse('products'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_product(self):
+        response = client.post(
+            reverse('products'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class updateSingleProductTest(TestCase):
+
+    def setUp(self):
+        self.test_item = Product.objects.create(
+            name="Test Item", quantity=3, price=70, category="Grocery")
+        self.valid_payload = {
+            "name": "Test Item",
+            "quantity": 4,
+            "price": 80,
+            "category": "Grocery"
+        }
+        self.invalid_payload = {
+            "name": "",
+            "quantity": 4,
+            "price": 70,
+            "category": "Grocery"
+        }
+
+    def test_valid_update_product(self):
+        response = client.put(
+            reverse('single-product', kwargs={'pk': self.test_item.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_product_update(self):
+        response = client.put(
+            reverse('single-product', kwargs={'pk': self.test_item.pk}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 class DeleteSingleProduct(TestCase):
-    # TODO
-    pass
+
+    def setUp(self):
+        self.item = Product.objects.create(
+            name="Test Delete Item", price=10, quantity=20, category="Electronics")
+
+    def test_valid_delete_product(self):
+        response = client.delete(
+            reverse('single-product', kwargs={'pk': self.item.pk})).json()
+        self.assertEqual(response['message'], f"Product with id {self.item.pk} has been deleted")
+
+    def test_invalid_delete_product(self):
+        random_uuid = str(uuid.uuid4())
+        response = client.delete(
+            reverse('single-product', kwargs={'pk': random_uuid})
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- api/views.py
+++ api/views.py
@@ -7,10 +7,18 @@ from products.models import Product
 @api_view(['GET', 'POST'])
 def products(request):
     if request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
+        data = {
+            'name': request.data.get('name'),
+            'price': int(request.data.get('price')),
+            'quantity': int(request.data.get('quantity')),
+            'category': request.data.get('category')
+        }
+        if request.data.get('description'):
+            data.update({'description': request.data['description']})
+        serializer = ProductSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         products = Product.objects.all()
@@ -22,17 +30,17 @@ def single_product(request, pk):
     try:
         product = Product.objects.get(id=pk)
     except Product.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ProductSerializer(product, many=False)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
